92ba782efaf4bd967665aec8337ffee60de620f8
Eurípedes <euripedeslipin@gmail.com>
2026-02-04 14:38:59 -0300

    feat: altera comprimento de senha para 20 caracteres

--- Application/DTO/Update/UsuarioUpdateDTO.cs
+++ Application/DTO/Update/UsuarioUpdateDTO.cs
@@ -14,7 +14,7 @@ namespace Application.DTO.Create
         [Required]
         [DataType(DataType.Password)]
         [MinLength(6, ErrorMessage = "Número mínimo de caracteres 6.")]
-        [MaxLength(50, ErrorMessage = "Número máximo de caracteres 50.")]
+        [MaxLength(50, ErrorMessage = "Número máximo de caracteres 20.")]
         [RegularExpression(
             @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]{6,}$",
              ErrorMessage = "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial."
